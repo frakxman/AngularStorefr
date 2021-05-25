822b5a99fac2ee17aff6467ef613e4e8b870e383
Fabian Rivera <frakxman@hotmail.com>
2021-05-25 18:55:24 -0500

    Change firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   url_api: 'https://platzi-store.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyABb9mUBGrxCy8RhSJWvqwaYSKuGRa7bSA',
-    authDomain: 'storefr-f825d.firebaseapp.com',
-    projectId: 'storefr-f825d',
-    storageBucket: 'storefr-f825d.appspot.com',
-    messagingSenderId: '37222674283',
-    appId: '1:37222674283:web:5e2b30c53b558b582b662e'
+    apiKey: 'AIzaSyAsvoqEA0RN_UiJ-VvLqo2cquEyxCNaaLY',
+    authDomain: 'angularstore-792b3.firebaseapp.com',
+    projectId: 'angularstore-792b3',
+    storageBucket: 'angularstore-792b3.appspot.com',
+    messagingSenderId: '638199613237',
+    appId: '1:638199613237:web:52bbf6af68c4790d7d6e7c'
   }
 };
